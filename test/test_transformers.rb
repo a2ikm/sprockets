c6aec673a1dc6c5b32f807e1d188a60023371021
Masato Ikeda <masato.ikeda@gmail.com>
2020-05-29 20:00:28 +0900

    add

--- test/test_transformers.rb
+++ test/test_transformers.rb
@@ -128,6 +128,9 @@ class TestTransformers < Sprockets::TestCase
     assert_equal [
       ['text/coffeescript', 1.0],
       ['application/coffeescript+ruby', 0.8],
+      ['text/eco', 0.8], # TODO: Extra step transform should be weighted down
+      ['text/ejs', 0.8], # TODO: Extra step transform should be weighted down
+      ['application/javascript+function', 0.8],
     ], @env.expand_transform_accepts(@env.parse_q_values('text/coffeescript'))
     assert_equal [['image/png', 1.0], ['image/svg+xml', 0.8]],
       @env.expand_transform_accepts(@env.parse_q_values('image/png'))
